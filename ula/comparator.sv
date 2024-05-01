89b7828d978ce8dbdaac956f526ec23915e69c63
Victor Lunarti Valadao <lunarti@gmail.com>
2024-05-01 18:24:26 -0300

    add structural comparator

--- ula/comparator.sv
+++ ula/comparator.sv
@@ -2,13 +2,13 @@ module comparator #(parameter width = 4)
 		   (input logic zero,carry,
 		    output logic hs,ls,hi,lo);
 
-  and gate_or1 (ls_or, zero, carry);
-  not gate_not1 (lo_not, carry);
-  not gate_not2 (hi_not, ls_or);
+  or gate_or1 (z_or_c, zero, carry);
+  not gate_not1 (not_c, carry);
+  not gate_not2 (not_z_or_c, z_or_c);
 
   assign hs = carry;
-  assign ls = ls_or;
-  assign hi = hi_not;
-  assign lo = lo_not;
+  assign ls = z_or_c;
+  assign hi = not_z_or_c;
+  assign lo = not_c;
 
 endmodule
